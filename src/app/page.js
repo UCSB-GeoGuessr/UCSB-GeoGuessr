fd960ddd05cccef3fcb1cf71b99021b68f1a87dc
Aden <adenjo@ucsb.edu>
2025-01-30 17:43:42 -0800

    Got barebone leafletMap showing up

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+"use client";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("./components/MapLeaflet"), { ssr: false });
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div>
+      <h1>My Leaflet Map</h1>
+      <Map />
+    </div>
+  )
 }
